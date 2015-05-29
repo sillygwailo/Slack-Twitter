f2cb4c49a9b2a73c1291c44ab3b5966504058412
Richard Eriksson <richard@justagwailo.com>
2015-05-29 14:15:56 -0700

    We don't need the 'type' property anymore.

--- plugins/filter/smartquotes.js
+++ plugins/filter/smartquotes.js
@@ -3,7 +3,6 @@ var filters = require(__dirname + '/../filters.js');
 var filter = {
   name: 'smartquotes',
   description: 'Replace quotes with smartquotes.',
-  type: 'filter',
   execute: function(text) {
     // Smart Quotes: http://www.leancrew.com/all-this/2010/11/smart-quotes-in-javascript/
     text = text.replace(/(^|[-\u2014\s(\["])'/g, "$1\u2018");       // opening singles
