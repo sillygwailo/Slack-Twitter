0881eff81026b8e4b53d73c8e31995a66244640f
Richard Eriksson <richard.eriksson@acquia.com>
2017-01-22 20:18:36 -0500

    Use the updated Slack client at slackapi/node-slack-sdk and update other dependencies.

--- slack-twitter.js
+++ slack-twitter.js
@@ -1,4 +1,8 @@
-var S = require('slack-client');
+var S = require('@slack/client');
+var RtmClient = S.RtmClient;
+var WebClient = S.WebClient;
+var CLIENT_EVENTS = S.CLIENT_EVENTS;
+
 var Twit = require('twit');
 var TwitterText = require('twitter-text');
 var U = require('url');
@@ -20,37 +24,68 @@ var slackOptions = {
   timeline_channel: process.env.SLACK_TIMELINE_CHANNEL
 }
 
-var Cl = new S(slackOptions.token, slackOptions.autoReconnect, slackOptions.autoMark);
-
 var fs = require('fs');
+var MemoryDataStore = S.MemoryDataStore;
+var rtm = new RtmClient(slackOptions.token, {
+    logLevel: 'error',
+    dataStore: new MemoryDataStore()
+  }
+);
 
-Cl.login();
-Cl.on('open', function() {
+var web = new WebClient(slackOptions.token);
+
+rtm.login();
+rtm.on('open', function() {
 
 });
-Cl.on('close', function () {
+rtm.on('close', function () {
   console.log('Connection closed, retrying...');
-  Cl.reconnect();
+  rtm.reconnect();
 });
 
 stream = T.stream('user');
 
 stream.on('tweet', function(tweet) {
-  var channel = Cl.getChannelByName(slackOptions.timeline_channel);
+  var channel = rtm.dataStore.getChannelByName(slackOptions.timeline_channel);
   if (typeof(tweet.retweeted_status) != 'undefined') {
-    channel.send('https://twitter.com/' + tweet.retweeted_status.user.screen_name + '/status/' + tweet.retweeted_status.id_str + ' RT by https://twitter.com/' + tweet.user.screen_name);
+    web.chat.postMessage(channel.id, 'https://twitter.com/' + tweet.retweeted_status.user.screen_name + '/status/' + tweet.retweeted_status.id_str + ' RT by https://twitter.com/' + tweet.user.screen_name, function(err, res) {
+      if (err) {
+        console.log('Error:', err);
+      } else {
+        console.log('Message sent: ', res);
+      }
+    });
     if (typeof(tweet.retweeted_status.quoted_status_id_str) != 'undefined') {
       T.get('/statuses/show/' + tweet.retweeted_status.quoted_status_id_str, {}, function(error, quoted_tweet, response) {
-        var channel = Cl.getChannelByName(slackOptions.timeline_channel);
-        channel.send('Quoted tweet inside RT: https://twitter.com/' + quoted_tweet.user.screen_name + '/status/' + quoted_tweet.id_str);
+        var channel = rtm.dataStore.getChannelByName(slackOptions.timeline_channel);
+        web.chat.postMessage(channel.id, 'Quoted tweet inside RT: https://twitter.com/' + quoted_tweet.user.screen_name + '/status/' + quoted_tweet.id_str, function(err, res) {
+          if (err) {
+            console.log('Error:', err);
+          } else {
+            console.log('Message sent: ', res);
+          }
+        });
       });
     }
   }
   else {
     if (typeof(tweet.user) != 'undefined') {
-      channel.send('https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str)
+      web.chat.postMessage(channel.id, 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str,
+      function(err, res) {
+        if (err) {
+          console.log('Error:', err);
+        } else {
+          console.log('Message sent: ', res);
+        }
+      });
       if (typeof(tweet.quoted_status) != 'undefined') {
-        channel.send('Quoted tweet: https://twitter.com/' + tweet.quoted_status.user.screen_name + '/status/' + tweet.quoted_status.id_str);
+        web.chat.postMessage(channel.id, 'Quoted tweet: https://twitter.com/' + tweet.quoted_status.user.screen_name + '/status/' + tweet.quoted_status.id_str, function(err, res) {
+          if (err) {
+            console.log('Error:', err);
+          } else {
+            console.log('Message sent: ', res);
+          }
+        });
       }
     }
   }
@@ -61,7 +96,7 @@ stream.on('error', function(error) {
   console.log('Twitter stream error: ' + error);
 });
 
-Cl.on('star_added', function(event) {
+rtm.on('star_added', function(event) {
   if (event.item.type == 'message') {
     path = U.parse(event.item.message.attachments[0].author_link).path.split('/');
     T.post('favorites/create', { id: path[3] }, function(error, data, response) {
@@ -72,7 +107,7 @@ Cl.on('star_added', function(event) {
     path = null;
   }
 });
-Cl.on('star_removed', function(event) {
+rtm.on('star_removed', function(event) {
   if (event.item.type == 'message') {
     path = U.parse(event.item.message.attachments[0].author_link).path.split('/');
     T.post('favorites/destroy', { id: path[3] }, function(error, data, response) {
@@ -83,8 +118,8 @@ Cl.on('star_removed', function(event) {
     path = null;
   }
 });
-Cl.on('message', function(message) {
-  the_channel = Cl.getChannelByName(slackOptions.post_channel);
+rtm.on('message', function(message) {
+  the_channel = rtm.dataStore.getChannelByName(slackOptions.post_channel);
   if (message.channel == the_channel.id && (message.subtype != 'message_changed' && message.subtype != 'bot_message' && message.subtype != 'channel_join')) {
     fs.readdir(__dirname + '/plugins/filter', function (error, files) {
       text = message.text;
@@ -103,8 +138,14 @@ Cl.on('message', function(message) {
         });
       }
       else {
-        channel = Cl.getChannelByID(message.channel);
-        channel.send("The tweet was too long! Character count: " + TwitterText.getTweetLength(message.text));
+        channel = rtm.dataStore.getChannelByID(message.channel);
+        web.chat.postMessage(channel.id, "The tweet was too long! Character count: " + TwitterText.getTweetLength(message.text), function(err, res) {
+          if (err) {
+            console.log('Error:', err);
+          } else {
+            console.log('Message sent: ', res);
+          }
+        });
         channel = null;
       } // If message longer than 140 character
     });
